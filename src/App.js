db81d3f0f2063aa8ff12cc408504f6c6a49fa15c
mahamudul5 <mh.bilash22@gmail.com>
2022-04-04 02:32:45 +0600

    component create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Blogs from './Component/Blogs/Blogs';
+import Home from './Component/Home/Home';
+import Navbar from './Component/navbar/Navbar';
+import Reviews from './Component/Reviews/Reviews';
+import Services from './Component/Services/Services';
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar></Navbar>
+      <Home></Home>
+      <Reviews></Reviews>
+      <Blogs></Blogs>
+      <Services></Services>
     </div>
   );
 }
