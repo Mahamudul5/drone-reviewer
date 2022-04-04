e682e0b7298d49ed547f7384953848f3a601360c
mahamudul5 <mh.bilash22@gmail.com>
2022-04-04 18:04:18 +0600

    recheart set up complete

--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -1,9 +1,78 @@
 import React from 'react';
+import './Dashboard.css'
+import { Bar, BarChart, CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
     return (
         <div>
-            <h1>this is dashboard</h1>
+            <h2>Our business summary</h2>
+            <div className='cheart'>
+                <div>
+                    <LineChart width={300} height={300} data={data}>
+                        <XAxis dataKey="month"></XAxis>
+                        <Tooltip></Tooltip>
+                        <YAxis dataKey="sell"></YAxis>
+                        <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+                        <Line type="monotone" dataKey="investment" stroke="#8884d8" />
+                        <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
+                        <Line type="monotone" dataKey="sell" stroke="#82ca9f" />
+                    </LineChart>
+                </div>
+                <div>
+                    <BarChart width={500} height={250} data={data}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+
+                        <Bar dataKey="sell" fill="#8884d8" />
+                        <Bar dataKey="investment" fill="#8884d8" />
+                        <Bar dataKey="revenue" fill="#82ca9d" />
+                        <Bar dataKey="month" fill="#82ca9d" />
+
+                    </BarChart>
+                </div>
+
+            </div>
+
 
         </div>
     );
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
                     <p>A leap forward in professional-quality aerial imaging, the Mavic 3 from DJI is their flagship drone that offers a dual-camera setup in a 3-axis gimbal. There's the 20MP wide-angle camera with 4/3" CMOS from legendary camera maker Hasselblad and a 12MP telephoto with 1/2" CMOS, 4K video, and 28x hybrid zoom.</p>
                     <button className='btn'>EXPLORE NOW</button>
                 </div>
-                <img src="./images/drone.jpg" alt="" />
+                <img src="./image/drone.jpg" alt="" />
             </div>
             <h1>Our Customer Reviews:({reviews.length})</h1>
 
